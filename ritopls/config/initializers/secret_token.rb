9c84a9928891e8cfebb8fa63d073f88b62867c95
benan789 <benjamin_an@yahoo.com>
2014-09-10 17:27:30 -0700

    Rails new

---
+++ ritopls/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ritopls::Application.config.secret_key_base = '56f699cd652f39853f574caa0b56a48641ac7482722b6a1eb99d21c919cad5ba34a2855f60e51141e736cbf675196837f406b7a34380a06e8212143ce2395bf0'
---
+++ ritopls/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ritopls::Application.config.session_store :cookie_store, key: '_ritopls_session'
